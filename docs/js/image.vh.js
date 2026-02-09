fe2bc9088b34eb843215885ef26a1ed0a490a6c1
Mega <etoakrasava@gmail.com>
2026-02-09 16:30:33 +0600

    Update image.vh.js

--- docs/js/image.vh.js
+++ docs/js/image.vh.js
@@ -1,1 +1,22 @@
- 
+// docs/js/image.vh.js
+export function initVH() {
+  const tg = window.Telegram?.WebApp;
+
+  function applyVH(){
+    if (tg && typeof tg.viewportHeight === "number") {
+      document.documentElement.style.setProperty("--vh", tg.viewportHeight + "px");
+    } else {
+      document.documentElement.style.setProperty("--vh", window.innerHeight + "px");
+    }
+  }
+
+  if (tg) {
+    tg.ready();
+    tg.expand();
+    applyVH();
+    tg.onEvent("viewportChanged", applyVH);
+  } else {
+    applyVH();
+    window.addEventListener("resize", applyVH);
+  }
+}
